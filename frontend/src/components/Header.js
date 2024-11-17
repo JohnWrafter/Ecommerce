f95e2059ab0523d0cee33204973deecf7aa24d17
John Wrafter <34818962+JohnWrafter@users.noreply.github.com>
2024-11-17 13:57:19 +0000

    update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Navbar, Nav, Container, Row } from 'react-bootstrap'
 
 function Header() {
   return (
-    <div>
-      <header>Header</header>
-    </div>
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+    <Container>
+        <Navbar.Brand href="/">ProShop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <nav className="ml-auto">
+                <Nav.Link href="/Cart">Cart</Nav.Link>
+                <Nav.Link href="/Login">Login</Nav.Link>
+            </nav>
+        </Navbar.Collapse>
+    </Container>
+</Navbar>
+    </header>
   )
 }
 
